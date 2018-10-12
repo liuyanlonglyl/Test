7046337b6582564298d4fc549a8bb151042fd366
mr.liu <1321576602@qq.com>
2018-10-12 16:01:34 +0800

    update in local

--- src/cn/hl/test/Test.java
+++ src/cn/hl/test/Test.java
@@ -3,5 +3,12 @@ package cn.hl.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		
+		test();
+	}
+
+	private static void test() {
+		// TODO Auto-generated method stub
+		
 	}
 }
