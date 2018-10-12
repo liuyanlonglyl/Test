8b9da1f72b73807c365114faaab76aae3eaf0e66
mr.liu <1321576602@qq.com>
2018-10-12 16:17:29 +0800

    123

--- src/cn/hl/test/Test2.java
+++ src/cn/hl/test/Test2.java
@@ -5,5 +5,11 @@ public class Test2 {
 		// TODO Auto-generated constructor stub
 		System.out.println("hello");
 		System.out.println("kkk");
+		test();
+	}
+
+	private void test() {
+		// TODO Auto-generated method stub
+		
 	}
 }
